93cd2c05b899b66ae37fe806337a1097c55f19b9
win-luck <chaso1495@naver.com>
2024-07-07 23:42:40 +0900

    [init] first commit

---
+++ src/main/java/gdsc/cau/puangbe/PuangbeApplication.java
@@ -0,0 +1,13 @@
+package gdsc.cau.puangbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuangbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuangbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdsc/cau/puangbe/PuangbeApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.cau.puangbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuangbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
