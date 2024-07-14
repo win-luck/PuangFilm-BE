67558940070ed6f9244bb06628b174558c39f1a5
pjw-redt <pjiww4@gmail.com>
2024-07-14 19:01:06 +0900

    [feat] DB 1차 구축

---
+++ src/main/java/gdsc/cau/puangbe/photo/entity/PhotoRequest.java
@@ -0,0 +1,37 @@
+package gdsc.cau.puangbe.photo.entity;
+
+import gdsc.cau.puangbe.common.enums.RequestStatus;
+import gdsc.cau.puangbe.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class PhotoRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "request_id")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "result_id")
+    private PhotoResult photoResult;
+
+    private RequestStatus status = RequestStatus.WAITING;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime updateDate;
+
+    @OneToMany(mappedBy = "request", cascade = CascadeType.REMOVE)
+    private List<PhotoOrigin> photoUrls = new ArrayList<>();
+}
---
+++ src/main/java/gdsc/cau/puangbe/user/entity/User.java
@@ -0,0 +1,35 @@
+package gdsc.cau.puangbe.user.entity;
+
+import gdsc.cau.puangbe.common.enums.Gender;
+import gdsc.cau.puangbe.photo.entity.PhotoRequest;
+import gdsc.cau.puangbe.photo.entity.PhotoResult;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String userName;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime requestDate;
+
+    private Gender gender;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<PhotoResult> photoResult = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<PhotoRequest> photoRequest = new ArrayList<>();
+}
