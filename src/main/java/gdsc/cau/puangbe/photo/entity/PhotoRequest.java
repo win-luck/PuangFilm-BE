799ce626f46dcd4286c966dd46d29ee8d82b80b5
pjw-redt <pjiww4@gmail.com>
2024-08-04 15:28:29 +0900

    [feat] photoRequest 생성자 추가 및 상태 관리 (#14)

--- src/main/java/gdsc/cau/puangbe/photo/entity/PhotoRequest.java
+++ src/main/java/gdsc/cau/puangbe/photo/entity/PhotoRequest.java
@@ -3,14 +3,19 @@ package gdsc.cau.puangbe.photo.entity;
 import gdsc.cau.puangbe.common.enums.Gender;
 import gdsc.cau.puangbe.common.enums.RequestStatus;
 import gdsc.cau.puangbe.user.entity.User;
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class PhotoRequest {
 
@@ -25,6 +30,7 @@ public class PhotoRequest {
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "result_id")
+    @Nullable
     private PhotoResult photoResult;
 
     private RequestStatus status;
@@ -35,6 +41,26 @@ public class PhotoRequest {
 
     private Gender gender;
 
-    @OneToMany(mappedBy = "request", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "request", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private List<PhotoOrigin> photoUrls = new ArrayList<>();
+
+    @Builder
+    public PhotoRequest(User user, Gender gender, List<String> urls){
+        this.user = user;
+        this.gender = gender;
+        this.status = RequestStatus.WAITING;
+        this.photoUrls = urls.stream().map(PhotoOrigin::new).toList();
+        this.createDate = LocalDateTime.now();
+        this.updateDate = LocalDateTime.now();
+    }
+
+    void processStatus(){
+        this.status = RequestStatus.PROCESSING;
+        this.updateDate = LocalDateTime.now();
+    }
+
+    void finishStatus() {
+        this.status = RequestStatus.FINISHED;
+        this.updateDate = LocalDateTime.now();
+    }
 }
