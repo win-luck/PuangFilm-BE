bcc53047de5ac4677e36b41d1c24fe46e438453e
win-luck <chaso1495@naver.com>
2024-08-04 19:14:39 +0900

    [chore] UploadImageDto 생성자 추가 (#15)

--- src/main/java/gdsc/cau/puangbe/photo/dto/request/UploadImageDto.java
+++ src/main/java/gdsc/cau/puangbe/photo/dto/request/UploadImageDto.java
@@ -1,10 +1,12 @@
 package gdsc.cau.puangbe.photo.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UploadImageDto {
     Long photoResultId;
     String imageUrl;
