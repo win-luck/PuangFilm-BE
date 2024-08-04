b51acbf0dd44cfabe95fd3ff72ec2ae719fa79ba
pjw-redt <pjiww4@gmail.com>
2024-08-04 15:26:49 +0900

    [chore] responseCode success 문구 rename

--- src/main/java/gdsc/cau/puangbe/photo/controller/PhotoController.java
+++ src/main/java/gdsc/cau/puangbe/photo/controller/PhotoController.java
@@ -15,19 +15,19 @@ public class PhotoController {
 
     @PostMapping("/{photoRequestId}")
     public ApiResponse<Long> createUploadRequest(@PathVariable Long photoRequestId) {
-        return ApiResponse.success(photoService.createPhoto(photoRequestId), ResponseCode.PHOTORESULT_CREATE_SUCCESS.getMessage());
+        return ApiResponse.success(photoService.createPhoto(photoRequestId), ResponseCode.PHOTO_RESULT_CREATE_SUCCESS.getMessage());
     }
 
     @PostMapping("/url")
     public ApiResponse<Void> uploadImage(@RequestBody UploadImageDto uploadImageDto) {
-        return ApiResponse.success(photoService.uploadPhoto(uploadImageDto.getPhotoResultId(), uploadImageDto.getImageUrl()), ResponseCode.PHOTORESULT_URL_UPLOADED.getMessage());
+        return ApiResponse.success(photoService.uploadPhoto(uploadImageDto.getPhotoResultId(), uploadImageDto.getImageUrl()), ResponseCode.PHOTO_RESULT_URL_UPLOADED.getMessage());
     }
 
     // TODO : 이메일 발송 관련 api 추가
 
     @GetMapping("/{imageId}")
     public ApiResponse<String> getImage(@PathVariable("imageId") Long photoRequestId) {
-        return ApiResponse.success(photoService.getPhotoUrl(photoRequestId), ResponseCode.PHOTORESULT_URL_FOUND.getMessage());
+        return ApiResponse.success(photoService.getPhotoUrl(photoRequestId), ResponseCode.PHOTO_RESULT_URL_FOUND.getMessage());
     }
 
 }
