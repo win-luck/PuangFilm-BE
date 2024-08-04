b3329c5df1b23354e734480d210297724687f3d8
pjw-redt <pjiww4@gmail.com>
2024-08-04 15:36:18 +0900

    [feat] RabbitMQ의 큐에 넣어줄 데이터 ImageInfo 생성 (#14)

---
+++ src/main/java/gdsc/cau/puangbe/photorequest/dto/ImageInfo.java
@@ -0,0 +1,22 @@
+package gdsc.cau.puangbe.photorequest.dto;
+
+import gdsc.cau.puangbe.common.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * AI 프로필 사진 생성 요청 시 RabbitMQ의 큐에 넣어줄 데이터
+ * 이를 통해 다시 파이썬에서 완성 API 호출 시 requestId를 받음으로써 DB에서 상태를 수정하고 Redis에서 삭제 가능
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageInfo {
+
+    List<String> photoOriginUrls;
+    Gender gender;
+    Long requestId;
+}
