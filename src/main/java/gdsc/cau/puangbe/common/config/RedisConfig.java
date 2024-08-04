8766fe70f1974fda78c54bb111b8769087ac31d4
win-luck <chaso1495@naver.com>
2024-08-04 20:41:33 +0900

    [build] RedisConfig 추가 및 자료구조 세팅 (#15)

---
+++ src/main/java/gdsc/cau/puangbe/common/config/RedisConfig.java
@@ -0,0 +1,43 @@
+package gdsc.cau.puangbe.common.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    // String key, Long value(requestId)를 다루기 위한 RedisTemplate입니다. (현재 유저의 요청의 대기열 역할을 수행하게 됩니다)
+    // opsForSet().add()를 통해 requestId를 추가할 수 있으며, 이 때 key는 별도로 만든 키로 설정합니다. (RequestQueue 등)
+    // 반드시 opsForSet() 연산을 해야만 Set 형태로 여러 requestId를 저장할 수 있다는 것에 유의해주세요.
+    @Bean
+    public RedisTemplate<String, Long> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Long> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+
+        // ObjectMapper 설정
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.activateDefaultTyping(
+                objectMapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL);
+
+        // GenericJackson2JsonRedisSerializer에 ObjectMapper 적용
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+        template.setValueSerializer(serializer);
+
+        return template;
+    }
+
+    // String key, String value를 다루기 위한 StringRedisTemplate입니다.
+    // opsForValue().set()을 통해 key-value를 저장할 수 있습니다. 필요할 때 사용하시면 됩니다.
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+}
