45b511304996022262fa504a6c3c76a6f31ab1d3
win-luck <chaso1495@naver.com>
2024-08-07 00:42:37 +0900

    [chore] ConstantUtil 추가 (#19)

---
+++ src/main/java/gdsc/cau/puangbe/common/util/ConstantUtil.java
@@ -0,0 +1,10 @@
+package gdsc.cau.puangbe.common.util;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ConstantUtil {
+
+    public static final String USER_ID_KEY = "photo-request-user";
+}
