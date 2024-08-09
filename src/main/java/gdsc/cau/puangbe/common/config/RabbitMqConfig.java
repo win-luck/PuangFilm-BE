1692bc9267fbb1d99ef9b34bb64a702dd0f8840b
rumos <gyunjin3@gmail.com>
2024-08-09 23:45:11 +0900

    [feat] rabbit mq config 설정 (#22)

---
+++ src/main/java/gdsc/cau/puangbe/common/config/RabbitMqConfig.java
@@ -0,0 +1,94 @@
+package gdsc.cau.puangbe.common.config;
+
+import gdsc.cau.puangbe.auth.config.RabbitMqProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.core.Queue;
+
+@RequiredArgsConstructor
+@Configuration
+public class RabbitMqConfig {
+    private final RabbitMqProperties rabbitMqProperties;
+
+    /**
+     * Exchange : Producer로부터 전달받은 메시지를 어떤 메시지 큐로 전송할 지 결정하는 장소
+     */
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    /**
+     * Routing : Exchange에서 해당하는 key에 맞게 Queue에 분배
+     */
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    /**
+     * Queue : Consumer가 소비하기 전까지 메시지가 보관되는 장소
+     */
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName);
+    }
+
+    /**
+     * 지정된 Exchange 이름으로 Direct Exchange Bean 을 생성
+     */
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    /**
+     * 주어진 Queue 와 Exchange 을 Binding 하고 Routing Key 을 이용하여 Binding Bean 생성
+     * Exchange 에 Queue 을 등록한다고 이해하자
+     **/
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    /**
+     * RabbitMQ 연동을 위한 ConnectionFactory 빈을 생성하여 반환
+     **/
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(rabbitMqProperties.getHost());
+        connectionFactory.setPort(rabbitMqProperties.getPort());
+        connectionFactory.setUsername(rabbitMqProperties.getUsername());
+        connectionFactory.setPassword(rabbitMqProperties.getPassword());
+        return connectionFactory;
+    }
+
+    /**
+     * RabbitTemplate
+     * ConnectionFactory 로 연결 후 실제 작업을 위한 Template
+     */
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    /**
+     * 직렬화(메세지를 JSON 으로 변환하는 Message Converter)
+     */
+    @Bean
+    public MessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
